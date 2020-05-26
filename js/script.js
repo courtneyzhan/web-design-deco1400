459e7331baec1131ee70588320033cb0b1e0756e
Zhimin Zhan <zhimin@itest2.com>
2020-05-26 21:42:26 +1000

    Scrollspy working and add some dummy data for Contact Us Page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready( function() {
-  console.log("Hello")
+  // console.log("Hello");
+  
+	$('.section').on('scrollSpy:enter', function() {
+    // console.log('enter:', $(this).attr('id'));
+    $('.local-navigation #' + $(this).attr('id') + "-link").addClass("active");
+	});
+
+	$('.section').on('scrollSpy:exit', function() {
+    // console.log('exit:', $(this).attr('id'));
+    $('.local-navigation #' + $(this).attr('id') + "-link").removeClass("active");
+	});
+
+  $.scrollSpy($('.section'))
+  
+  
 });
