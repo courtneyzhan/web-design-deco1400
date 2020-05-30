c79a36287c588ccc32994a7bc9312feb4aed0a3b
Zhimin Zhan <zhimin@itest2.com>
2020-05-30 20:59:32 +1000

    FAQ done, animation for chevron working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(document).ready( function() {
-  // console.log("Hello");
+  //
+  $('.faq-icon').click(function() {
+    console.log("Hello!!")
+    $(this).parent().parent().parent().find('p').toggle(400, "swing", null);
+    // $(this).css("color", "red");
+    $(this).toggleClass("down")
+    // $(this).toggleClass("fa-rotate-90");
+  })
   
+  // ScrollSpy for local navigation
 	$('.section').on('scrollSpy:enter', function() {
     // console.log('enter:', $(this).attr('id'));
     $('.local-navigation #' + $(this).attr('id') + "-link").addClass("active");
